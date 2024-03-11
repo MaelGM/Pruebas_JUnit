8118bede471885ddb2b8db67a97e43af42ab7557
mael <mael.grandguillot@gmail.com>
2024-03-11 22:43:05 +0100

    Clases finalizadas

--- Practica2/src/es/iesmz/tests/DiasLluvia.java
+++ Practica2/src/es/iesmz/tests/DiasLluvia.java
@@ -27,6 +27,51 @@ public class DiasLluvia {
         return cont;
     }
 
+    public int trimestreLluvioso(){
+        int cont1 = 0, cont2 = 0, cont3 =0, cont4 =0, max = 0;
+        for (int i = 0; i <calendario.length ; i++) {
+            if (i<=2) {
+                for (int j = 0; j < calendario[i].length; j++) {
+                    if (calendario[i][j]) cont1++;
+                }
+                if (cont1 > max) max = cont1;
+            }
+
+            if (i>=3 && i<=5) {
+                for (int j = 0; j < calendario[i].length; j++) {
+                    if (calendario[i][j]) cont2++;
+                }
+                if (cont2 > max) max = cont2;
+            }
+
+            if (i>=6 && i<=8) {
+                for (int j = 0; j < calendario[i].length; j++) {
+                    if (calendario[i][j]) cont3++;
+                }
+                if (cont3 > max) max = cont3;
+            }
+
+            if (i>=9 && i<=11) {
+                for (int j = 0; j < calendario[i].length; j++) {
+                    if (calendario[i][j]) cont4++;
+                }
+                if (cont4 > max) max = cont4;
+            }
+        }
+        return max;
+    }
+
+    public int primerDiaLluvia(){
+        int dias = 0;
+        for (int i = 0; i < calendario.length; i++) {
+            for (int j = 0; j < calendario[i].length; j++) {
+                dias++;
+                if (calendario[i][j]) return dias;
+            }
+        }
+        return -1;
+    }
+
     private boolean validarFecha(int dia, int mes){
         if ((dia < 31 && dia >= 0) && (mes >= 0 && mes < 12)){
             if (mes == 1 && dia >= 29) return false;
