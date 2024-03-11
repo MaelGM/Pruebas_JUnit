d93cc23534aa8b117442c4de05e3ce7ba7445b06
Mael <mael.grandguillot@gmail.com>
2024-03-11 10:45:33 +0100

    Segundo commit

--- Practica2/src/es/iesmz/tests/DiasLluvia.java
+++ Practica2/src/es/iesmz/tests/DiasLluvia.java
@@ -5,15 +5,32 @@ public class DiasLluvia {
 
 
     public boolean registroDia(int dia, int mes, boolean lluvia){
-        if ((dia <= 31 && dia > 0) || (mes > 0 && mes <= 12) ){
-            calendario[mes][dia] = lluvia;
+        if (validarFecha(dia-1, mes-1)){
+            calendario[mes-1][dia-1] = lluvia;
             return true;
         }else return false;
     }
 
     public boolean consultarDia(int dia, int mes){
-        if ((dia <= 31 && dia > 0) || (mes > 0 && mes <= 12) ){
-            return calendario[mes][dia];
+        if (validarFecha(dia-1, mes-1)){
+            return calendario[mes-1][dia-1];
+        }else return false;
+    }
+
+    public int contarDiasLluviosos(){
+        int cont = 0;
+        for (int i = 0; i < calendario.length; i++) {
+            for (int j = 0; j < calendario[0].length; j++) {
+                if (calendario[i][j]) cont++;
+            }
+        }
+        return cont;
+    }
+
+    private boolean validarFecha(int dia, int mes){
+        if ((dia < 31 && dia >= 0) && (mes >= 0 && mes < 12)){
+            if (mes == 1 && dia >= 29) return false;
+            return true;
         }else return false;
     }
 }
--- Practica2/src/es/iesmz/tests/DiasLluviaTest.java
+++ Practica2/src/es/iesmz/tests/DiasLluviaTest.java
@@ -2,12 +2,39 @@ package es.iesmz.tests;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class DiasLluviaTest {
     @Test
     void registrarDia(){
         DiasLluvia calendario = new DiasLluvia();
-        calendario.registroDia(1,12,true);
-        assertEquals(true, calendario.consultarDia(1,12));
+        calendario.registroDia(31,5,true);
+        assertEquals(true, calendario.consultarDia(31,5));
+    }
+
+    @Test
+    void registrarDiaFebrero(){
+        DiasLluvia calendario = new DiasLluvia();
+        assertEquals(false, calendario.registroDia(30,2, true));
+    }
+
+    @Test
+    void registrarDiasLluviosos(){
+        DiasLluvia calendario = new DiasLluvia();
+        calendario.registroDia(7,2, true);
+        calendario.registroDia(9,5, true);
+        calendario.registroDia(2,13, true);
+        calendario.registroDia(30,2, true);
+        assertEquals(2, calendario.contarDiasLluviosos());
+    }
+
+    @Test
+    void registrarDiasLluviososFebrero(){
+        DiasLluvia calendario = new DiasLluvia();
+        calendario.registroDia(29,2, false);
+        calendario.registroDia(15,2, true);
+        calendario.registroDia(-5,2, true);
+        calendario.registroDia(30,2, true);
+        assertEquals(1, calendario.contarDiasLluviosos());
     }
 }
