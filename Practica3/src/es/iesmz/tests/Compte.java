d416621981c9ab3ba09037765d758404880daa83
Mael <mael.grandguillot@gmail.com>
2024-03-12 13:37:05 +0100

    Segundo commit - ("generaIBAN" finalizado)

--- Practica3/src/es/iesmz/tests/Compte.java
+++ Practica3/src/es/iesmz/tests/Compte.java
@@ -8,12 +8,32 @@ public class Compte {
     public boolean compruebaIBAN(String anaves){
         boolean pais = false, digitos = false;
         if (anaves.substring(0,2).equalsIgnoreCase("ES")) pais = true;
-        BigInteger numeros = new BigInteger(anaves.substring(4)+"142800");
-        BigInteger num = new BigInteger("97");
-        BigInteger resto = numeros.mod(num);
-
-        if (98-resto.intValue() == Integer.parseInt(anaves.substring(2,4))) digitos = true;
+        try{
+            BigInteger numeros = new BigInteger(anaves.substring(4)+"142800");
+            BigInteger num = new BigInteger("97");
+            BigInteger resto = numeros.mod(num);
+            if (98-resto.intValue() == Integer.parseInt(anaves.substring(2,4))) digitos = true;
+        }catch (NumberFormatException | ArithmeticException e){
+            e.printStackTrace();
+            return false;
+        }
 
         return pais && digitos;
+    }
+
+    public String generaIBAN(String entitat, String oficina, String dc, String compte){
+        BigInteger numeros;
+        String nums = entitat+oficina+dc+compte;
+        if (nums.length() != 20) return null;
+        try{
+            numeros = new BigInteger(entitat+oficina+dc+compte+"142800");
+        }catch (NumberFormatException | ArithmeticException e){
+            e.printStackTrace();
+            return null;
+        }
+        BigInteger resto = numeros.mod(new BigInteger("97"));
+        String codigo = String.valueOf(98-resto.intValue());
+
+        return "ES"+codigo+nums;
     }
 }
--- Practica3/src/es/iesmz/tests/CompteTest.java
+++ Practica3/src/es/iesmz/tests/CompteTest.java
@@ -10,25 +10,21 @@ public class CompteTest {
         Compte c = new Compte();
         assertEquals(true, c.compruebaIBAN("ES6621000418401234567891"));
     }
-
     @Test
     void compruebaIBAN_2(){
         Compte c = new Compte();
         assertEquals(true, c.compruebaIBAN("ES6000491500051234567892"));
     }
-
     @Test
     void compruebaIBAN_3(){
         Compte c = new Compte();
         assertEquals(true, c.compruebaIBAN("ES9420805801101234567891"));
     }
-
     @Test
     void compruebaIBAN_4(){
         Compte c = new Compte();
         assertEquals(false, c.compruebaIBAN("ES7600246912501234567891"));
     }
-
     @Test
     void compruebaIBAN_5(){
         Compte c = new Compte();
@@ -38,6 +34,38 @@ public class CompteTest {
     void compruebaIBAN_6(){
         Compte c = new Compte();
         assertEquals(false, c.compruebaIBAN("ES8200491500051234567892"));
+    }
+
+
+    @Test
+    void generaIBAN_1(){
+        Compte c = new Compte();
+        assertEquals("ES7100302053091234567895", c.generaIBAN("0030","2053","09","1234567895"));
+    }
+    @Test
+    void generaIBAN_1(){
+        Compte c = new Compte();
+        assertEquals("ES7100302053091234567895", c.generaIBAN("0030","2053","09","1234567895"));
+    }
+    @Test
+    void generaIBAN_1(){
+        Compte c = new Compte();
+        assertEquals("ES7100302053091234567895", c.generaIBAN("0030","2053","09","1234567895"));
+    }
+    @Test
+    void generaIBAN_1(){
+        Compte c = new Compte();
+        assertEquals("ES7100302053091234567895", c.generaIBAN("0030","2053","09","1234567895"));
+    }
+    @Test
+    void generaIBAN_1(){
+        Compte c = new Compte();
+        assertEquals("ES7100302053091234567895", c.generaIBAN("0030","2053","09","1234567895"));
+    }
+    @Test
+    void generaIBAN_1(){
+        Compte c = new Compte();
+        assertEquals("ES7100302053091234567895", c.generaIBAN("0030","2053","09","1234567895"));
     }
 
 }
